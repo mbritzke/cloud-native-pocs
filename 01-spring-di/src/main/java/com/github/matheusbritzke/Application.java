178518bb3d9425e826eb619ea27d32dc25a82d38
mbritzke <matheusbritzke@gmail.com>
2018-03-06 15:00:46 -0300

    Atualizado com mais exemplos de operações

--- 01-spring-di/src/main/java/com/github/matheusbritzke/Application.java
+++ 01-spring-di/src/main/java/com/github/matheusbritzke/Application.java
@@ -10,7 +10,10 @@ public class Application {
     public static void main(String args[]){
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         Calculator currentCalculator = (Calculator) applicationContext.getBean("calculator");
-        System.out.println(currentCalculator.calculate(3,1, "+"));
-        System.out.println(currentCalculator.calculate(2, 2,"-"));
+        System.out.println("3+1 = " + currentCalculator.calculate(3,1, "+"));
+        System.out.println("2-2 = " + currentCalculator.calculate(2, 2,"-"));
+        System.out.println("3*3 = " + currentCalculator.calculate(3, 3,"*"));
+        System.out.println("16/4 = " + currentCalculator.calculate(16, 4,"/"));
+        System.out.println("5^ = " + currentCalculator.calculate(5, 3,"^"));
     }
 }
