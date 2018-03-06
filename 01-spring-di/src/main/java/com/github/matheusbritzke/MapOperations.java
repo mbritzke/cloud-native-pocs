0c3772245f28d2dad4e20f1a8cbab03606863004
mbritzke <matheusbritzke@gmail.com>
2018-03-06 14:24:14 -0300

    Classe para mapear as operações da calculadora

---
+++ 01-spring-di/src/main/java/com/github/matheusbritzke/MapOperations.java
@@ -0,0 +1,13 @@
+package com.github.matheusbritzke;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapOperations {
+
+    private Map<String, Operation> mapOperation = new HashMap<>();
+
+    public Map<String, Operation> getMapOperation() {
+        return mapOperation;
+    }
+}
