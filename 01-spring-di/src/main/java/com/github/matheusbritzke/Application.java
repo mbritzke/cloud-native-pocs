0899b9920f56dd1fc88e36a42e5888519152ad99
mbritzke <matheusbritzke@gmail.com>
2018-03-07 13:06:13 -0300

    Exemplo de histórico de operações

--- 01-spring-di/src/main/java/com/github/matheusbritzke/Application.java
+++ 01-spring-di/src/main/java/com/github/matheusbritzke/Application.java
@@ -10,10 +10,15 @@ public class Application {
     public static void main(String args[]){
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         Calculator currentCalculator = (Calculator) applicationContext.getBean("calculator");
+
+        System.out.println("OPERATIONS: ");
         System.out.println("3+1 = " + currentCalculator.calculate(3,1, "+"));
         System.out.println("2-2 = " + currentCalculator.calculate(2, 2,"-"));
         System.out.println("3*3 = " + currentCalculator.calculate(3, 3,"*"));
         System.out.println("16/4 = " + currentCalculator.calculate(16, 4,"/"));
-        System.out.println("5^ = " + currentCalculator.calculate(5, 3,"^"));
+        System.out.println("5^3 = " + currentCalculator.calculate(5, 3,"^"));
+
+        System.out.println("LIST OF ALL OPERATIONS: ");
+        currentCalculator.showHistory();
     }
 }
