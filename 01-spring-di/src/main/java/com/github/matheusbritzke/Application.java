7264ca77ebf09059d49555eebd84be4eef0e00dc
mbritzke <matheusbritzke@gmail.com>
2018-03-07 14:02:37 -0300

    Exemplo de operação inválida

--- 01-spring-di/src/main/java/com/github/matheusbritzke/Application.java
+++ 01-spring-di/src/main/java/com/github/matheusbritzke/Application.java
@@ -12,13 +12,15 @@ public class Application {
         Calculator currentCalculator = (Calculator) applicationContext.getBean("calculator");
 
         System.out.println("OPERATIONS: ");
-        System.out.println("3+1 = " + currentCalculator.calculate(3,1, "+"));
-        System.out.println("2-2 = " + currentCalculator.calculate(2, 2,"-"));
-        System.out.println("3*3 = " + currentCalculator.calculate(3, 3,"*"));
-        System.out.println("16/4 = " + currentCalculator.calculate(16, 4,"/"));
-        System.out.println("5^3 = " + currentCalculator.calculate(5, 3,"^"));
+        System.out.println("3+1 : \n" + currentCalculator.calculate(3,1, "+"));
+        System.out.println("2-2 : \n" + currentCalculator.calculate(2, 2,"-"));
+        System.out.println("3*3 : \n" + currentCalculator.calculate(3, 3,"*"));
+        System.out.println("16/4 : \n" + currentCalculator.calculate(16, 4,"/"));
+        System.out.println("5^3 : \n" + currentCalculator.calculate(5, 3,"^"));
+        System.out.println("10c3 : \n" + currentCalculator.calculate(10, 3,"c"));
 
-        System.out.println("LIST OF ALL OPERATIONS: ");
+        System.out.println("\nLIST OF ALL OPERATIONS: ");
         currentCalculator.showHistory();
+
     }
 }
