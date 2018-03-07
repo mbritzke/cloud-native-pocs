1839b40440505d7ff52b5a694550810b564f0585
mbritzke <matheusbritzke@gmail.com>
2018-03-07 11:05:21 -0300

    Classe para o histórico de operações

---
+++ 01-spring-di/src/main/java/com/github/matheusbritzke/maps/HistoryOperations.java
@@ -0,0 +1,14 @@
+package com.github.matheusbritzke.maps;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HistoryOperations {
+
+    private Map<String, List> historyOperations = new HashMap<>();
+
+    public Map<String, List> getHistoryOperations() {
+        return historyOperations;
+    }
+}
