4a2fb8e769076be666207540d2de6dedd2381870
mbritzke <matheusbritzke@gmail.com>
2018-03-07 13:04:37 -0300

    Funções para salvar as operações e as imprimir na tela

--- 01-spring-di/src/main/java/com/github/matheusbritzke/calculator/Calculator.java
+++ 01-spring-di/src/main/java/com/github/matheusbritzke/calculator/Calculator.java
@@ -1,16 +1,31 @@
 package com.github.matheusbritzke.calculator;
 
+import com.github.matheusbritzke.maps.HistoryOperations;
 import com.github.matheusbritzke.maps.MapOperations;
 
 public class Calculator {
 
     private MapOperations mapOperations;
+    private HistoryOperations historyOperations;
 
-    public Calculator(MapOperations mapOperations) {
+    public Calculator(MapOperations mapOperations, HistoryOperations historyOperations) {
         this.mapOperations = mapOperations;
+        this.historyOperations = historyOperations;
     }
 
     public double calculate(double firstNumber, double secondNumber, String operation){
-        return mapOperations.getMapOperation().get(operation).calculate(firstNumber, secondNumber);
+        double answer = mapOperations.getMapOperation().get(operation).calculate(firstNumber, secondNumber);
+        saveOperation(firstNumber, secondNumber, operation, answer);
+        return answer;
+    }
+
+    private void saveOperation(double firstNumber, double secondNumber, String operation, double ans){
+        String completeOperation = firstNumber + " "+ operation + " " + secondNumber + " = " + ans;
+        historyOperations.getHistoryOperations().get(operation).add(completeOperation);
+    }
+
+    public void showHistory(){
+        for(String aux: historyOperations.getHistoryOperations().keySet())
+            System.out.println(historyOperations.getHistoryOperations().get(aux) + "\n");
     }
 }
