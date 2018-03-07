671d6971f2a1e5d6b8f872aae13ef2a0db3b0342
mbritzke <matheusbritzke@gmail.com>
2018-03-07 14:23:33 -0300

    Atualização dos exemplos

--- 01-spring-di/src/main/java/com/github/matheusbritzke/Application.java
+++ 01-spring-di/src/main/java/com/github/matheusbritzke/Application.java
@@ -12,12 +12,13 @@ public class Application {
         Calculator currentCalculator = (Calculator) applicationContext.getBean("calculator");
 
         System.out.println("OPERATIONS: ");
-        System.out.println("3+1 : \n" + currentCalculator.calculate(3,1, "+"));
-        System.out.println("2-2 : \n" + currentCalculator.calculate(2, 2,"-"));
-        System.out.println("3*3 : \n" + currentCalculator.calculate(3, 3,"*"));
-        System.out.println("16/4 : \n" + currentCalculator.calculate(16, 4,"/"));
-        System.out.println("5^3 : \n" + currentCalculator.calculate(5, 3,"^"));
-        System.out.println("10c3 : \n" + currentCalculator.calculate(10, 3,"c"));
+        System.out.println("a) 3+1 : \n" + currentCalculator.calculate(3,1, "+"));
+        System.out.println("b) 2-2 : \n" + currentCalculator.calculate(2, 2,"-"));
+        System.out.println("c) 3*3 : \n" + currentCalculator.calculate(3, 3,"*"));
+        System.out.println("d) 16/4 : \n" + currentCalculator.calculate(16, 4,"/"));
+        System.out.println("e) 5^3 : \n" + currentCalculator.calculate(5, 3,"^"));
+        System.out.println("f) 10c3 : \n" + currentCalculator.calculate(10, 3,"c"));
+        System.out.println("g) 15/0 : \n" + currentCalculator.calculate(15, 0,"/"));
 
         System.out.println("\nLIST OF ALL OPERATIONS: ");
         currentCalculator.showHistory();
