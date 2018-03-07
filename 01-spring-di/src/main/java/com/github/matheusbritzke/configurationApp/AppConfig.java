2342e5e727f5d6682092e10f9c387602cf7e5bcb
mbritzke <matheusbritzke@gmail.com>
2018-03-07 13:03:26 -0300

    Bean para o histórico de operações

--- 01-spring-di/src/main/java/com/github/matheusbritzke/configurationApp/AppConfig.java
+++ 01-spring-di/src/main/java/com/github/matheusbritzke/configurationApp/AppConfig.java
@@ -2,18 +2,21 @@ package com.github.matheusbritzke.configurationApp;
 
 import com.github.matheusbritzke.calculator.Calculator;
 import com.github.matheusbritzke.functions.*;
+import com.github.matheusbritzke.maps.HistoryOperations;
 import com.github.matheusbritzke.maps.MapOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
 
 @Configuration
 @ComponentScan(basePackages = "com.github.matheusbritzke")
 public class AppConfig {
 
     @Bean
-    public Calculator calculator(MapOperations mapOperations){
-        Calculator calculator = new Calculator(mapOperations);
+    public Calculator calculator(MapOperations mapOperations, HistoryOperations historyOperations){
+        Calculator calculator = new Calculator(mapOperations, historyOperations);
         return calculator;
     }
 
@@ -26,5 +29,16 @@ public class AppConfig {
         mapOperations.getMapOperation().put("/", new Division());
         mapOperations.getMapOperation().put("^", new Pow());
         return mapOperations;
+    }
+
+    @Bean
+    public HistoryOperations historyOperations(){
+        HistoryOperations historyOperations = new HistoryOperations();
+        historyOperations.getHistoryOperations().put("+", new ArrayList<>());
+        historyOperations.getHistoryOperations().put("-", new ArrayList<>());
+        historyOperations.getHistoryOperations().put("*", new ArrayList<>());
+        historyOperations.getHistoryOperations().put("/", new ArrayList<>());
+        historyOperations.getHistoryOperations().put("^", new ArrayList<>());
+        return historyOperations;
     }
 }
